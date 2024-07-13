5299356440174ca9965fedef75c18534d5803802
Vladimir Khapaev <khapaev@mail.ru>
2024-07-14 00:27:38 +0300

    some changes main

--- main.py
+++ main.py
@@ -300,13 +300,13 @@ class MyServer(BaseHTTPRequestHandler):
                     <form>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1"
+                            <input name="email" type="email" class="form-control" id="exampleInputEmail1"
                                    aria-describedby="emailHelp">
                             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
                         </div>
                         <div class="mb-3 form-check">
                             <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -384,40 +384,10 @@ class MyServer(BaseHTTPRequestHandler):
 </html>
         """
 
-    def __get_article_content(self, page_address):
-        if page_address == 'news1':
-            return 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. '
-        elif page_address == 'news2':
-            return 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'
-        elif page_address == 'news3':
-            return 'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
-
-        return 'Article not found!'
-
-    def __get_blog_article(self, page_address):
-        return f"""
-        <html><head>
-        <title>Blog</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        </head><body class="container">
-        <div class="card">
-        <div class="card-header">
-        <a class="btn btn-primary" href="/">Back</a>
-        </div>
-        <div class="card-body">
-        <p>{self.__get_article_content(page_address)}</p>
-        </div>
-        </div>
-        </body>
-        </html>
-        """
-
     def do_GET(self):
         query_components = parse_qs(urlparse(self.path).query)
-        page_address = query_components.get('page')
+        print(query_components)
         page_content = self.__get_index()
-        if page_address:
-            page_content = self.__get_blog_article(page_address[0])
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
